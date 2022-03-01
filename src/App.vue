f8afecb8c9679b27ecfe6e03645c7137334557dc
Daniel Kelly <me@danielkelly.io>
2022-03-01 17:32:20 -0600

    remove entry list (unneeded abstraction that will hinder lesson flow)

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
-import EntryList from "./components/EntryList.vue";
+import EntryCard from "@/components/EntryCard.vue";
 </script>
 
 <template>
   <main class="container m-auto p-10">
     <TheHeader />
     <EntryEditor />
-    <EntryList />
+    <ul>
+      <li>
+        <EntryCard />
+      </li>
+    </ul>
   </main>
 </template>
